71beb3bbbf21a676dffc131db87c78a8f16c9643
Sikandar Saleem <sikandarsaleem2000@gmail.com>
2023-09-22 22:50:44 +0500

    Add: Dark/Light Mode Toggler

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,18 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+export default function ColorModeSwitch() {
+  // These are defined variable and metod in this hook
+  // that automatically toggle between light/dark mode
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+        colorScheme="green"
+      />
+      <Text>{colorMode === "light" ? "Dark" : "Light"} Mode</Text>
+    </HStack>
+  );
+}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 export default function NavBar() {
   return (
-    <HStack>
+    <HStack justify="space-between" padding="0 10px">
       <Image src={logo} boxSize="60px" />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 }
